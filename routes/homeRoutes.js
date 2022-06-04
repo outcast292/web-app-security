b304840a56e63069e5c43c47e2b6cf0e6e1d6fed
oumaima-lm <oumaima.lmhamdi@gmail.com>
2022-06-04 22:51:25 +0100

    file upload layout & logic

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -6,28 +6,39 @@ const db = mysql.createConnection({
     host: "localhost",
     password: "",
     database: "secwebdb",
+});
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./images/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
   });
 
-router.get('/', (req, res)=>{
+const upload = multer({ storage: storage }); //multer : midlleware for uploading files
+
+router.get('/', (req, res) => {
     db.query("SELECT * FROM clients", (err, result) => {
         if (err) {
             console.log(err);
         } else {
-            res.render('index', {result});
+            res.render('index', { result });
             console.log(result);
         }
     });
 })
-router.post('/', (req, res) => {
+router.post('/',upload.single("file"), (req, res) => {
 
     const code = req.body.code;
     const nom = req.body.nom;
     const prenom = req.body.prenom;
     const address = req.body.address;
     const telephone = req.body.telephone;
-
+    const image = req.body.image;
     db.query(
-        `INSERT INTO clients (code, nom, prenom, address, telephone) VALUES ("${code}","${nom}","${prenom}", "${address}", "${telephone}");`,
+        `INSERT INTO clients (code, nom, prenom, address, telephone, image) VALUES ("${code}","${nom}","${prenom}", "${address}", "${telephone}", "${image}");`,
         (err, result) => {
             if (err) {
                 console.log(err);
@@ -46,8 +57,9 @@ router.post("/modify", (req, res) => {
     const prenom = req.body.prenom;
     const address = req.body.address;
     const telephone = req.body.telephone;
+    const image = req.body.image;
     db.query(
-        `UPDATE clients SET code="${code}", nom = "${nom}", prenom = "${prenom}", address="${address}", telephone = "${telephone}" WHERE nom = "${nom}"`,
+        `UPDATE clients SET code="${code}", nom = "${nom}", prenom = "${prenom}", address="${address}", telephone = "${telephone}, image = "${image}" WHERE nom = "${nom}"`,
         (err, result) => {
             if (err) {
                 console.log(err);
