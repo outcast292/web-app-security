42679204737c91fd94d9a9c4d6e1d477d9f377ad
oumaima-lm <oumaima.lmhamdi@gmail.com>
2022-06-09 20:32:51 +0100

    adding code to prevent from sql injection vulnerability

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -6,27 +6,45 @@ const mysql = require("mysql");
 //     host: "localhost",
 //     password: "",
 //     database: "secwebdb",
-//   });
+// });
 const db = mysql.createConnection({
     user: "upwehzfnqs9cmgec",
     host: "bohojaiuimzrml7fwjmt-mysql.services.clever-cloud.com",
     password: "QQfitjOXIZ4kX3wdkU7W",
     database: "bohojaiuimzrml7fwjmt",
 });
-  router.get('/', (req, res)=>{
-    res.render('client', {result: null});
+router.get('/', (req, res) => {
+    res.render('client', { result: null });
 })
 
-router.post('/search', (req, res)=>{
+router.post('/search', (req, res) => {
     const nom = req.body.nom;
-    db.query(`SELECT * FROM clients JOIN commandes ON clients.nom=commandes.nomclient AND clients.nom="${nom}"`, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('client', {result});
-            console.log(result);
-        }
-    });
+    db.query(`SELECT * FROM clients JOIN commandes ON clients.nom=commandes.nomclient AND clients.nom=:name`,
+        {name:nom},
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('client', { result });
+                console.log(result);
+            }
+        });
 })
+/*
+we can also use this syntaxe to escape SQL injection
+router.post('/search', (req, res) => {
+    const nom = req.body.nom;
+    db.query(`SELECT * FROM clients JOIN commandes ON clients.nom=commandes.nomclient AND clients.nom=:name`,
+        {name:nom},
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('client', { result });
+                console.log(result);
+            }
+        });
+})
+*/
 
 module.exports = router;
